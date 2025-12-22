fb23c7a5b1b7f3b64c545d008ef1813f69b6d54e
Bayu Official <anosvoldigoard02@gmail.com>
2025-12-22 14:34:43 +0700

    Update bmkg.js

--- service/bmkg.js
+++ service/bmkg.js
@@ -1,38 +1,54 @@
-const axios = require('axios');
-const cheerio = require('cheerio');
+import axios from "axios"
+import cheerio from "cheerio"
 
+/* ===============================
+   GEMPA TERKINI (BMKG)
+   ASLI DARI FILE LU
+================================ */
 async function gempa() {
-	return new Promise(async(resolve,reject) => {
-		axios.get('https://www.bmkg.go.id/gempabumi/gempabumi-dirasakan.bmkg')
-		.then(({ data }) => {
-			const $ = cheerio.load(data)
-			const drasa = [];
-			$('table > tbody > tr:nth-child(1) > td:nth-child(6) > span').get().map((rest) => {
-				dir = $(rest).text();
-				drasa.push(dir.replace('\t', ' '))
-			})
-			teks = ''
-			for(let i=0; i<drasa.length; i++){
-				teks += drasa[i] + '\n'
-			}
-			const rasa = teks
-			const format = {
-				imagemap : $('div.modal-body > div > div:nth-child(1) > img').attr('src'),
-				magnitude : $('table > tbody > tr:nth-child(1) > td:nth-child(4)').text(),
-				kedalaman: $('table > tbody > tr:nth-child(1) > td:nth-child(5)').text(),
-				wilayah: $('table > tbody > tr:nth-child(1) > td:nth-child(6) > a').text(),
-				waktu: $('table > tbody > tr:nth-child(1) > td:nth-child(2)').text(),
-				lintang_bujur: $('table > tbody > tr:nth-child(1) > td:nth-child(3)').text(),
-				dirasakan: rasa
-			}
-			const result = {
-				source: 'www.bmkg.go.id',
-				data: format
-			}
-			resolve(result)
-		})
-		.catch(reject)
-	})
-};
+  return new Promise(async (resolve, reject) => {
+    axios
+      .get("https://www.bmkg.go.id/gempabumi/gempabumi-dirasakan.bmkg")
+      .then(({ data }) => {
+        const $ = cheerio.load(data)
+        const drasa = []
 
-module.exports =  gempa;
+        $("table > tbody > tr:nth-child(1) > td:nth-child(6) > span")
+          .get()
+          .map((rest) => {
+            const dir = $(rest).text()
+            drasa.push(dir.replace("\t", " "))
+          })
+
+        let rasa = ""
+        for (let i = 0; i < drasa.length; i++) {
+          rasa += drasa[i]
+        }
+
+        const format = {
+          tanggal: $("table > tbody > tr:nth-child(1) > td:nth-child(1)").text(),
+          waktu: $("table > tbody > tr:nth-child(1) > td:nth-child(2)").text(),
+          lintang_bujur: $("table > tbody > tr:nth-child(1) > td:nth-child(3)").text(),
+          magnitude: $("table > tbody > tr:nth-child(1) > td:nth-child(4)").text(),
+          kedalaman: $("table > tbody > tr:nth-child(1) > td:nth-child(5)").text(),
+          dirasakan: rasa,
+        }
+
+        const result = {
+          source: "www.bmkg.go.id",
+          data: format,
+        }
+
+        resolve(result)
+      })
+      .catch(reject)
+  })
+}
+
+/* ===============================
+   HANDLER (WAJIB BUAT API ROUTE)
+================================ */
+export async function handler() {
+  // BMKG ga butuh query
+  return await gempa()
+}
