0db56d58d0b5337c8db1b3f4c615e62c6c4bbfdb
Bayu Official <anosvoldigoard02@gmail.com>
2025-12-22 08:18:42 +0700

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,17 +1,19 @@
 import "./globals.css"
 import Navbar from "@/components/Navbar"
+import BannerPopup from "@/components/BannerPopup"
 import Footer from "@/components/Footer"
 
 export const metadata = {
-  title: "Sayaka Miki Rest Api",
+  title: "Sayaka Miki | Rest Api",
   description: "This rest api is free to use, use a limit so the server doesn't go down"
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className="bg-black text-white">
         <Navbar />
+        <BannerPopup />
         {children}
         <Footer />
       </body>
